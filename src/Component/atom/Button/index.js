0deec81ead4d8b8f7e94d7343600dba2fa262b22
Ronald <ronaldgipa9419@gmail.com>
2020-06-08 19:15:37 +0800

    sample

--- src/Component/atom/Button/index.js
+++ src/Component/atom/Button/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from './style.module.scss'
 
-export default ({ className = null, label }) => (
+export default ({ className = null, label, ok }) => (
   <button className={`${className} ${style.btn}`}>{label}</button>
 )
