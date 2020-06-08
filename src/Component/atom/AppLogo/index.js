444afba0723e5cb93d48e400a6927244c4ecc57a
Ronald <ronaldgipa9419@gmail.com>
2020-06-08 19:48:28 +0800

    Sample

--- src/Component/atom/AppLogo/index.js
+++ src/Component/atom/AppLogo/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import style from './style.module.scss'
 
+let y = 'asd'
+let x = y
+
 export default ({ className = null, src }) => (
   <img className={`${className} ${style.logo}`} {...{ src }} />
 )
